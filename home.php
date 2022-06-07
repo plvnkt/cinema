abc039f7260e62f51af1dfc24fc11b92d875ef03
plvnkt <kubapla99@gmail.com>
2022-06-08 01:04:15 +0200

    basic navbar

--- home.php
+++ home.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
+    <!-- do ustalenia nazwa -->
+    <title>Kino Flex</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="image/icon.png">
 </head>
 <body>
-   
+    <header>
+        <div class="navbar">
+            <a href="home.php" class="logo">Flex</a>
+            <a href="films.php" class="nav">Repertuar</a>
+            <a href="about.php" class="nav">O nas</a>
+            <a href="contact.php" class="nav">Kontakt</a>
+        </div>
+    </header>
+
+
+
+
+
+
+<script src="js/script.js"></script>
 </body>
 </html>
