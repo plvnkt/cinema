d87a1c1aecbc8d34173307e9a2c6db1d5a612908
plvnkt <kubapla99@gmail.com>
2022-06-16 15:13:25 +0200

    Dodanie osobnego footer i navbar

--- home.php
+++ home.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
+     <!-- navbar -->
     <?php
         include_once 'header.php';
     ?>
@@ -90,7 +91,7 @@
             </div>
         </div>
     </section>
-    
+
     <!-- Footer  -->
     <?php
         include_once 'footer.php';
